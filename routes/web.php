3f1a2e219a90aca777ad058150e5c73079635c28
OmarAsem27 <omarasem6215@gmail.com>
2025-04-20 14:46:03 +0200

    feat: integrate Laravel Reverb with real-time progress bar using Bootstrap - Installed Laravel Reverb for WebSocket communication - Created a test command to broadcast progress updates - Installed Bootstrap and implemented a progress bar UI - Connected command input to real-time progress bar updates via Reverb

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Events\ChangeProgressValue;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('reverb-example', function ($hello) {
+//     return event(ChangeProgressValue::class);
+// });
